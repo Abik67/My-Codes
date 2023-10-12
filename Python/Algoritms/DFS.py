8e2905cbb1fbb0accfa527aa63f4f61ab85e659b
Abik Alam <107752127+Abik67@users.noreply.github.com>
2023-10-12 19:18:31 +0600

    Add files via upload

---
+++ Python/Algoritms/DFS.py
@@ -0,0 +1,19 @@
+visited = set()
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(visited, graph, neighbor)
+
+print("Following is the Depth-First Search")
+graph = {
+    '5' : ['3','7'],
+    '3' : ['2', '4'],
+    '7' : ['8'],
+    '2' : [],
+    '4' : ['8'],
+    '8' : []
+}
+dfs(visited, graph, '5')
